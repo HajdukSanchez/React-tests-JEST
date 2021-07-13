f2e81703d73a85455df4a2df79e4a7223e5b0429
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-12 19:22:30 -0500

    :white_check_mark: [ADD] Boolean, Arrays and Callbacks tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,8 +1,39 @@
 // This is a test file for the global tests
 
-/* Text for compare and test */
+/* Variables for compare and do the tests */
 const text = 'hello world'
+const fruits = ['apple', 'pineapple', 'orange']
 
-test('should contain a text', () => {
+test('should the variable contain a text', () => {
   expect(text).toMatch(/world/)
+})
+
+test('should the array contains an apple', () => {
+  expect(fruits).toContain('apple')
+})
+
+test('should be greater than', () => {
+  expect(10).toBeGreaterThan(9)
+})
+
+test('should be true', () => {
+  expect(true).toBeTruthy()
+})
+
+test('should be false', () => {
+  expect(false).toBeFalsy()
+})
+
+/* callbacks test */
+const reverseString = (string, callback) => {
+  // This function convert to string something that receives
+  callback(string.split('').reverse().join(''))
+}
+
+test('should the callback work', () => {
+  expect(
+    reverseString('hello', (string) => {
+      expect(string).toBe('olleh') // This is a reverse string expected
+    })
+  )
 })
