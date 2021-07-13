16d3d79259baf835aa9f6d4b93b6066597375475
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-12 19:33:11 -0500

    :white_check_mark: [ADD] promises, async, after and before tests

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -19,7 +19,6 @@ test('should be greater than', () => {
 test('should be true', () => {
   expect(true).toBeTruthy()
 })
-
 test('should be false', () => {
   expect(false).toBeFalsy()
 })
@@ -29,11 +28,32 @@ const reverseString = (string, callback) => {
   // This function convert to string something that receives
   callback(string.split('').reverse().join(''))
 }
-
-test('should the callback work', () => {
+test('should the callback works', () => {
   expect(
     reverseString('hello', (string) => {
       expect(string).toBe('olleh') // This is a reverse string expected
     })
   )
 })
+
+/* Promises */
+const reverseString2 = (string) => {
+  return new Promise((resolve, reject) => {
+    if (!string) reject(new Error('Error'))
+    resolve(string.split('').reverse().join(''))
+  })
+}
+test('should the promise works', () => {
+  return reverseString2('hello').then((string) => {
+    expect(string).toBe('olleh')
+  })
+})
+test('should async/await works', async () => {
+  const string = await reverseString2('hello')
+  expect(string).toBe('olleh')
+})
+
+afterEach(() => console.log('After each test'))
+afterAll(() => console.log('After all tests'))
+beforeEach(() => console.log('Before each test'))
+beforeAll(() => console.log('Before all tests'))
